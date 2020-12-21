9476b6ea187ad0ef2a2f1dc599a70bb0030dc974
Matheus Cintra <matheus.cintra@synvia.com>
2020-12-21 19:25:04 -0300

    feat: ensure LoginController call Authentication with correct data

---
+++ src/presentation/controllers/login/login.ts
@@ -0,0 +1,43 @@
+import { Authentication } from '../../../domain/usecases/authentication'
+import { InvalidParamError, MissingParamError } from '../../errors'
+import { badRequest, serverError } from '../../helpers/http-helper'
+import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+import { EmailValidator } from '../signup/signup-protocols'
+
+export class LoginController implements Controller {
+  private readonly emailValidator: EmailValidator
+  private readonly authentication: Authentication
+
+  constructor (emailValidator: EmailValidator, authentication: Authentication) {
+    this.emailValidator = emailValidator
+    this.authentication = authentication
+  }
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const requiredFields = ['email', 'password']
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return await new Promise(resolve => resolve(badRequest(new MissingParamError(field))))
+        }
+      }
+
+      const isValid = this.emailValidator.isValid(httpRequest.body.email)
+
+      if (!isValid) {
+        return badRequest(new InvalidParamError('email'))
+      }
+
+      const { email, password } = httpRequest.body
+
+      await this.authentication.auth(email, password)
+
+      return {
+        statusCode: 200,
+        body: {}
+      }
+    } catch (error) {
+      return serverError(error)
+    }
+  }
+}
