a9337d9575ac8eb7871bac5783d79c8d2947522a
Matheus Cintra <matheus.cintra@synvia.com>
2021-01-18 15:15:56 -0300

    test: ensure ValidationComposite not return if validation succeeds

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -42,4 +42,13 @@ describe('Validation Composite', () => {
     const error = sut.validate({ field: 'any_value' })
     expect(error).toEqual(new Error())
   })
+
+  test('Should not return if Validation succeeds', () => {
+    const { sut } = makeSut()
+    const error = sut.validate({
+      field: 'any_value',
+      fieldToCompare: 'any_value'
+    })
+    expect(error).toBeNull()
+  })
 })
