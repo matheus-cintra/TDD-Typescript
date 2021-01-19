aca5cb0fcdb24b4b56b52c7983a36b00c33a55c8
Matheus Cintra <matheus.cintra@synvia.com>
2021-01-19 15:10:50 -0300

    refactor: change Authentication to receive a model

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -8,11 +8,12 @@ import {
 import { MissingParamError, ServerError } from '../../errors'
 import { HttpRequest, Authentication } from './login-protocols'
 import { Validation } from '../signup/signup-protocols'
+import { AuthenticationModel } from '../../../domain/usecases/authentication'
 
 describe('Login Controller', () => {
   const makeAuthentication = (): Authentication => {
     class AuthenticationStub implements Authentication {
-      async auth (email: string, password: string): Promise<string> {
+      async auth (authentication: AuthenticationModel): Promise<string> {
         return await new Promise((resolve) => resolve('any_token'))
       }
     }
@@ -53,7 +54,10 @@ describe('Login Controller', () => {
     const { sut, authenticationStub } = makeSut()
     const authSpy = jest.spyOn(authenticationStub, 'auth')
     await sut.handle(makeFakeRequest())
-    expect(authSpy).toHaveBeenCalledWith('any_email@teste.com', 'any_password')
+    expect(authSpy).toHaveBeenCalledWith({
+      email: 'any_email@teste.com',
+      password: 'any_password'
+    })
   })
 
   test('Should return 401 if invalid credentials provided', async () => {
--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -29,22 +29,9 @@ export class LoginController implements Controller {
         return badRequest(error)
       }
 
-      // const requiredFields = ['email', 'password']
-      // for (const field of requiredFields) {
-      //   if (!httpRequest.body[field]) {
-      //     return badRequest(new MissingParamError(field))
-      //   }
-      // }
-
-      // const isValid = this.emailValidator.isValid(httpRequest.body.email)
-
-      // if (!isValid) {
-      //   return badRequest(new InvalidParamError('email'))
-      // }
-
       const { email, password } = httpRequest.body
 
-      const accessToken = await this.authentication.auth(email, password)
+      const accessToken = await this.authentication.auth({ email, password })
 
       if (!accessToken) {
         return unauthorized()
