90f0abea674ae686f3b773165a5aa657362e5fef
Matheus Cintra <matheus.cintra@synvia.com>
2021-01-19 21:08:56 -0300

    feat: initiate authentication usecase

---
+++ src/data/usecases/authentication/db-authentication.spec1.ts
@@ -0,0 +1,8 @@
+// describe('DBAuthentication UseCase', () => {
+//   test('Should call LoadAccountByEmailRepository with correct email ', async () => {
+//     const sut = new DbAuthentication()
+//     const loadSpy = jest.spyOn(loadAccountByEmailRepository, 'load')
+//     await sut.auth({ email: 'any_email@mail.com', password: 'any_password' })
+//     expect(loadSpy).toHaveBeenCalledWith('any_email@mail.com')
+//   })
+// })
